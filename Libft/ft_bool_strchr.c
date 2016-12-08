026a7f8d2ce96edc832d9ab4d4c8b54bfd8313c7
Julien DEBLADIS <julien.debladis@gmail.com>
2016-12-08 14:07:45 +0100

    Thu Dec  8 13:51:58 CET 2016

--- Libft/ft_bool_strchr.c
+++ Libft/ft_bool_strchr.c
@@ -6,24 +6,24 @@
 /*   By: jdebladi <jdebladi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/08 13:35:54 by jdebladi          #+#    #+#             */
-/*   Updated: 2016/12/08 13:40:14 by jdebladi         ###   ########.fr       */
+/*   Updated: 2016/12/08 14:07:25 by jdebladi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int ft_bool_strchr(const char *s, int c)
+int	ft_bool_strchr(const char *s, int c)
 {
-    size_t  i;
-    char    *tmp;
+	size_t	i;
+	char	*tmp;
 
-    i = 0;
-    tmp = (char *)s;
-    while (tmp[i])
-    {
-        if (tmp[i] == (char)c)
-            return (1);
-        i++;
-    }
-    return (0);
+	i = 0;
+	tmp = (char *)s;
+	while (tmp[i])
+	{
+		if (tmp[i] == (char)c)
+			return (1);
+		i++;
+	}
+	return (0);
 }
--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jdebladi <jdebladi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/04 18:32:18 by jdebladi          #+#    #+#             */
-/*   Updated: 2016/12/08 13:47:47 by jdebladi         ###   ########.fr       */
+/*   Updated: 2016/12/08 14:07:01 by jdebladi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,7 +53,7 @@ void			*ft_memcpy(void *dst, const void *src, size_t n);
 void			ft_memdel(void **ap);
 void			*ft_memmove(void *dst, const void *src, size_t len);
 void			*ft_memset(void *b, int c, size_t len);
-void 			ft_occurence(int n, char **argv);
+void			ft_occurence(int n, char **argv);
 void			ft_putchar(char c);
 void			ft_putchar_fd(char c, int fd);
 void			ft_putendl(char const *s);
