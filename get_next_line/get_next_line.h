0850d2a07bcd270aa8d8442e6299cf69d5747b6c
Julien DEBLADIS <julien.debladis@gmail.com>
2016-12-12 17:30:34 +0100

    Mon Dec 12 16:25:16 CET 2016

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdebladi <jdebladi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/11/16 19:14:49 by jdebladi          #+#    #+#             */
+/*   Updated: 2016/12/02 14:11:00 by jdebladi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <string.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft/libft.h"
+# define BUFF_SIZE 32
+
+int		get_next_line(const int fd, char **line);
+
+#endif
