0c1a3048387aac0eaf35394b3e75f558fdd242cb
Julien DEBLADIS <jdebladi@e1r2p18.42.fr>
2017-03-22 12:40:03 +0100

    jdebladi Wed Mar 22 11:32:24 CET 2017

--- Libft/libft.h
+++ Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jdebladi <jdebladi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/04 18:32:18 by jdebladi          #+#    #+#             */
-/*   Updated: 2017/03/17 12:42:56 by jdebladi         ###   ########.fr       */
+/*   Updated: 2017/03/22 12:06:30 by jdebladi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,28 @@
 # include <float.h>
 
 # define BUFF_SIZE 32
+# define BOL "\x1B[01m"
+# define FAI "\x1B[02m"
+# define ITA "\x1B[03m"
+# define UDL "\x1B[04m"
+# define BLK "\x1B[30m"
+# define RED "\x1B[31m"
+# define GRN "\x1B[32m"
+# define YEL "\x1B[33m"
+# define BLU "\x1B[34m"
+# define MAG "\x1B[35m"
+# define CYN "\x1B[36m"
+# define WHT "\x1B[37m"
+# define BBLK "\x1B[40m"
+# define BRED "\x1B[41m"
+# define BGRN "\x1B[42m"
+# define BYEL "\x1B[43m"
+# define BBLU "\x1B[44m"
+# define BMAG "\x1B[45m"
+# define BCYN "\x1B[46m"
+# define BWHT "\x1B[47m"
+# define OVR "\x1B[53m"
+# define RES "\x1B[00m"
 
 typedef struct	s_list
 {
