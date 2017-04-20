bc62eeca9c0ed31666b9c50d8d07eb9e728b69e4
Julien DEBLADIS <jdebladi@e3r6p8.42.fr>
2017-04-20 12:44:12 +0200

    jdebladi Thu Apr 20 09:31:30 CEST 2017

--- ft_printf/Libft/ft_tabdel.c
+++ ft_printf/Libft/ft_tabdel.c
@@ -1,39 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_endless_strjoin.c                               :+:      :+:    :+:   */
+/*   ft_tabdel.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jdebladi <jdebladi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/03/26 23:12:08 by jdebladi          #+#    #+#             */
-/*   Updated: 2017/03/26 23:29:29 by jdebladi         ###   ########.fr       */
+/*   Created: 2017/04/14 09:19:16 by jdebladi          #+#    #+#             */
+/*   Updated: 2017/04/14 17:12:06 by jdebladi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "includes/Libft.h"
+#include "includes/libft.h"
 
-char	*ft_endless_strjoin(int nbr, ...)
+void	ft_tabdel(char **tab)
 {
 	int i;
-	va_list args;
-	char *ret;
-	char *tmp;
 
-	i = 0;
-	va_start(args, nbr);
-	ret = malloc(sizeof(char));
-	while (i < nbr)
+	if (tab != NULL)
 	{
-		tmp = ret;
-		free(tmp);
-		if (!(tmp = va_arg(args, char *)))
-			{
-				ft_putendl_fd("Error: nbr too big", 2);
-				exit(0);
-			}
-		ret = ft_strjoin(ret, tmp);
-		i++;
+		i = -1;
+		while (tab[++i])
+			free(tab[i]);
+		free(tab[i]);
+		free(tab);
 	}
-	va_end(args);
-	return (ret);
 }
--- ft_printf/Libft/libft.h
+++ ft_printf/Libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jdebladi <jdebladi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/04 18:32:18 by jdebladi          #+#    #+#             */
-/*   Updated: 2017/03/17 12:42:56 by jdebladi         ###   ########.fr       */
+/*   Updated: 2017/04/20 12:27:16 by jdebladi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,32 @@
 # include <string.h>
 # include <unistd.h>
 # include <float.h>
+# include <stdarg.h>
 
 # define BUFF_SIZE 32
+# define CLN "\x1B[K"
+# define BOL "\x1B[01m"
+# define FAI "\x1B[02m"
+# define ITA "\x1B[03m"
+# define UDL "\x1B[04m"
+# define BLK "\x1B[30m"
+# define RED "\x1B[31m"
+# define GRN "\x1B[32m"
+# define YEL "\x1B[33m"
+# define BLU "\x1B[34m"
+# define MAG "\x1B[35m"
+# define CYN "\x1B[36m"
+# define WHT "\x1B[37m"
+# define BBLK "\x1B[40m"
+# define BRED "\x1B[41m"
+# define BGRN "\x1B[42m"
+# define BYEL "\x1B[43m"
+# define BBLU "\x1B[44m"
+# define BMAG "\x1B[45m"
+# define BCYN "\x1B[46m"
+# define BWHT "\x1B[47m"
+# define OVR "\x1B[53m"
+# define RES "\x1B[00m"
 
 typedef struct	s_list
 {
@@ -97,6 +121,7 @@ int				ft_strncmp(const char *s1, const char *s2, size_t n);
 char			*ft_strncpy(char *dst, const char *src, size_t len);
 int				ft_strnequ(char const *s1, char const *s2, size_t n);
 char			*ft_strnew(size_t size);
+char			*ft_strnjoin(int nbr, ...);
 char			*ft_strnstr(const char *big, const char *little, size_t len);
 char			*ft_strrchr(const char *s, int c);
 char			*ft_strrealloc(char *s, size_t size);
@@ -106,6 +131,7 @@ char			*ft_strstr(const char *big, const char *little);
 char			*ft_strsub(char const *s, unsigned int start, size_t len);
 char			*ft_strtrim(char const *s);
 char			**ft_tab(size_t col, size_t line);
+void			ft_tabdel(char **tab);
 int				ft_tolower(int c);
 int				ft_toupper(int c);
 int				ft_wcbytesize(wchar_t wc);
