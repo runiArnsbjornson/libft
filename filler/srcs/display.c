c809443d6c9f1651db4f597a5d55de0a4a670584
Julien DEBLADIS <jdebladi@e3r6p8.42.fr>
2017-04-23 16:51:48 +0200

    jdebladi Sun Apr 23 10:07:45 CEST 2017

---
+++ filler/srcs/display.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   display.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jdebladi <jdebladi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/21 09:54:15 by jdebladi          #+#    #+#             */
+/*   Updated: 2017/04/23 16:31:16 by jdebladi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/filler.h"
+
+void	init_display(t_display *color)
+{
+	color->grn = 0;
+	color->red = 0;
+	color->wht = 0;
+}
+
+void	display_percent(t_data *data, t_display *color)
+{
+	int total;
+
+	total = data->board->x * data->board->y;
+	ft_putstr_fd(BRED"  "RES RED" = ", 2);
+	ft_putnbr_fd(color->red * 100 / total, 2);
+	ft_putstr_fd("% (", 2);
+	ft_putnbr_fd(color->red, 2);
+	ft_putstr_fd(" spaces)", 2);
+	ft_putstr_fd("\t"RES BGRN"  "RES GRN" = ", 2);
+	ft_putnbr_fd(color->grn * 100 / total, 2);
+	ft_putstr_fd("% (", 2);
+	ft_putnbr_fd(color->grn, 2);
+	ft_putstr_fd(" spaces)", 2);
+	ft_putstr_fd("\t"RES BWHT"  "RES WHT" empty spaces = ", 2);
+	ft_putnbr_fd(color->wht, 2);
+	ft_putstr_fd("\n\n", 2);
+	usleep(100000 - total > 0 ? 100000 - total : 0);
+}
+
+void	display_color(t_data *data, t_pos *pos, t_display *color)
+{
+	if (data->board->board[pos->y][pos->x] == P1)
+	{
+		color->grn++;
+		write(2, BGRN"  "RES, 12);
+	}
+	else if (data->board->board[pos->y][pos->x] == P2)
+	{
+		color->red++;
+		write(2, BRED"  "RES, 12);
+	}
+	else
+	{
+		color->wht++;
+		write(2, BWHT"  "RES, 12);
+	}
+}
+
+void	display_graph(t_data *data)
+{
+	t_pos		pos;
+	t_display	color;
+
+	pos.y = 0;
+	init_display(&color);
+	ft_putstr_fd(TCLN, 2);
+	while (pos.y < data->board->y)
+	{
+		pos.x = 0;
+		while (pos.x < data->board->x)
+		{
+			display_color(data, &pos, &color);
+			++pos.x;
+		}
+		ft_putchar_fd('\n', 2);
+		++pos.y;
+	}
+	ft_putchar_fd('\n', 2);
+	display_percent(data, &color);
+	init_display(&color);
+}
