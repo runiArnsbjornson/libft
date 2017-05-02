4265286494c2ca7609f55669efdccdc9e26965aa
runiArnsbjornson <julien.debladis@gmail.com>
2017-05-02 20:34:38 +0200

    mardi

--- Libft/ft_memccpy.c
+++ Libft/ft_memccpy.c
@@ -14,15 +14,11 @@
 
 void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
-	size_t	i;
+	void *p;
 
-	i = 0;
-	while (i < n)
-	{
-		((char *)dst)[i] = ((char *)src)[i];
-		if (((unsigned char *)src)[i] == ((unsigned char)c))
-			return (&(dst[i + 1]));
-		i++;
-	}
+	p = ft_memchr(src, c, n);
+	if (p != NULL)
+		return (ft_memcpy(dst, src, p - src + 1));
+	ft_memcpy(dst, src, n);
 	return (NULL);
 }
