cea717edfc1763c2ffafeb85b0f655b913eaea7b
Julien DEBLADIS <jdebladi@e3r6p8.42.fr>
2017-05-08 12:38:47 +0200

    jdebladi Mon May  8 10:13:47 CEST 2017

--- ft_strchr.c
+++ ft_strchr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jdebladi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: jdebladi <jdebladi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/06 13:58:57 by jdebladi          #+#    #+#             */
-/*   Updated: 2016/11/10 13:57:33 by jdebladi         ###   ########.fr       */
+/*   Updated: 2017/05/08 11:47:27 by jdebladi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,13 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	size_t	i;
-	char	*tmp;
+	const char ch = c;
 
-	i = 0;
-	tmp = (char *)s;
-	while (tmp[i] && tmp[i] != c)
-		i++;
-	if (tmp[i] == c)
-		return (&tmp[i]);
-	else
-		return (NULL);
+	while (*s != ch)
+	{
+		if (*s == '\0')
+			return (NULL);
+		s++;
+	}
+	return ((char *)s);
 }
